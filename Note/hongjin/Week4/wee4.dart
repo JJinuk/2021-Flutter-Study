2bd686e588b0efc3660e854af935b4aef15a72cd
hongjin4790 <904rla@naver.com>
2021-10-26 19:15:13 +0900

    Hongjin2021GDSC Flutter Study- week4

---
+++ Note/hongjin/Week4/wee4.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: ' app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First app'),
+      ),
+      body: Center(
+        child: Column(
+          children: [Text('hello'), Text('hello'), Text('hello')],
+        ),
+      ),
+    );
+  }
+}
