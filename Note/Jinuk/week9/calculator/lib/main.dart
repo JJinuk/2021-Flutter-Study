0f79211ee8d1bfe9d8b57376aa3df8b821973956
Jinuk <jitoor610@gmail.com>
2021-11-25 23:30:34 +0900

    [jinuk] 2021 GDSC Flutter Study - week9_addNewCalc
    Signed-off-by: Jinuk <jitoor610@gmail.com>

--- Note/Jinuk/week9/calculator/lib/main.dart
+++ Note/Jinuk/week9/calculator/lib/main.dart
@@ -1,39 +1,139 @@
 import 'package:flutter/material.dart';
-import 'widgets/calc_button.dart';
 
 void main() {
-  runApp(calcApp());
+  runApp(MyApp());
 }
 
-class calcApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
-      home: Scaffold(
-        backgroundColor: Color(0xFF283637),
-        body: Center(
-          child: Row(
-            children: <Widget>[
-              CalcButton(
-                text: 'AC',
-                fillColor: 0xFF6C807F
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator Neumorphism',
+      home: CalculatorNeuApp(),
+    );
+  }
+}
+
+const Color colorDark = Color(0xFF374352);
+const Color colorLight = Color(0xFFe6eeff);
+
+class CalculatorNeuApp extends StatefulWidget {
+  CalculatorNeuApp({Key? key}) : super(key: key);
+
+  @override
+  _CalculatorNeuAppState createState() => _CalculatorNeuAppState();
+}
+
+class _CalculatorNeuAppState extends State<CalculatorNeuApp> {
+  bool darkMode = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkMode ? colorDark : colorLight,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                child: Text('Result'),
+              ),
+              Container(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonRounded(title: '0'),
+                      ],
+                    )
+                  ],
                 ),
-              CalcButton(
-                text: 'C',
-                fillColor: 0xFF6C807F
-                ),
-              CalcButton(
-                text: '%',
-                fillColor: 0xFF6C807F
-                ),
-              CalcButton(
-                text: '/',
-                fillColor: 0xFF6C807F
-                ),
-            ]),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+
+    // ignore: dead_code
+    Widget _buttonRounded({required String title, double padding = 20}) {
+      return NeuContainer(
+        darkMode: darkMode,
+        borderRadius: BorderRadius.circular(40),
+        child: Container(
+          width: padding * 2,
+          height: padding * 2,
+          child: Center(child: Text('$'),)
         ),
       );
+    }
+  }
+}
+
+class NeuContainer extends StatefulWidget {
+  final bool darkMode;
+  final Widget child;
+  final BorderRadius borderRadius;
+  final EdgeInsetsGeometry padding;
+
+  NeuContainer(
+      {this.darkMode = false,
+      required this.child,
+      required this.borderRadius,
+      required this.padding});
+  @override
+  _NeuContainerState createState() => _NeuContainerState();
+}
+
+class _NeuContainerState extends State<NeuContainer> {
+  bool _isPressed = false;
+
+  void _onPointerDown(PointerDownEvent event) {
+    setState(() {
+      _isPressed = true;
+    });
+  }
+
+  void _onPointerUp(PointerUpEvent event) {
+    setState(() {
+      _isPressed = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool darkMode = widget.darkMode;
+    return Listener(
+      onPointerDown: _onPointerDown,
+      onPointerUp: _onPointerUp,
+      child: Container(
+        padding: widget.padding,
+        decoration: BoxDecoration(
+            color: darkMode ? colorDark : colorLight,
+            borderRadius: widget.borderRadius,
+            boxShadow: _isPressed
+                ? null
+                : [
+                    BoxShadow(
+                      color:
+                          darkMode ? Colors.black54 : Colors.blueGrey.shade200,
+                      offset: Offset(4.0, 4.0),
+                      blurRadius: 15.0,
+                      spreadRadius: 1.0,
+                    ),
+                    BoxShadow(
+                        color:
+                            darkMode ? Colors.blueGrey.shade700 : Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0)
+                  ]),
+        child: widget.child,
+      ),
+    );
   }
 }
