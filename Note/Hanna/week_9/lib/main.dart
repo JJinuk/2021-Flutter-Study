7940de613ef76a0a5bcbba4596f23a63d08ffae4
hanna8254 <90447035+hanna8254@users.noreply.github.com>
2021-11-26 20:43:17 +0900

    Add files via upload

---
+++ Note/Hanna/week_9/lib/main.dart
@@ -0,0 +1,212 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Calculator',
+      home: CalculatorNeuApp(),
+    );
+  }
+}
+
+const Color colorDark = Color(0xFF374352);
+const Color colorLight = Color(0xFFe6eeff);
+
+class CalculatorNeuApp extends StatefulWidget {
+  @override
+  _CalculatorNeuAppState createState() => _CalculatorNeuAppState();
+}
+
+class _CalculatorNeuAppState extends State<CalculatorNeuApp> {
+  bool darkMode = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: darkMode ? colorDark : colorLight,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(18),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                color: Colors.red,
+                child: Text('Result'),
+              ),
+              Container(
+                child: Column(children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buttonRounded(
+                          title: 'C',
+                          textColor:
+                              darkMode ? Colors.green : Colors.redAccent),
+                      _buttonRounded(title: '('),
+                      _buttonRounded(title: ')'),
+                      _buttonRounded(
+                          title: '/',
+                          textColor: darkMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buttonRounded(title: '7'),
+                      _buttonRounded(title: '8'),
+                      _buttonRounded(title: '9'),
+                      _buttonRounded(
+                          title: 'x',
+                          textColor: darkMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buttonRounded(title: '4'),
+                      _buttonRounded(title: '5'),
+                      _buttonRounded(title: '6'),
+                      _buttonRounded(
+                          title: '-',
+                          textColor: darkMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buttonRounded(title: '1'),
+                      _buttonRounded(title: '2'),
+                      _buttonRounded(title: '3'),
+                      _buttonRounded(
+                          title: '+',
+                          textColor: darkMode ? Colors.green : Colors.redAccent)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buttonRounded(title: '0'),
+                      _buttonRounded(title: ','),
+                      _buttonRounded(
+                          icon: Icons.backspace_outlined,
+                          iconColor:
+                              darkMode ? Colors.green : Colors.redAccent),
+                      _buttonRounded(
+                          title: '=',
+                          textColor: darkMode ? Colors.green : Colors.redAccent)
+                    ],
+                  )
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonRounded(
+      {String? title,
+      double padding = 17,
+      IconData? icon,
+      Color? iconColor,
+      Color? textColor}) {
+    return NeuContainer(
+      darkMode: darkMode,
+      borderRadius: BorderRadius.circular(40),
+      padding: EdgeInsets.all(padding),
+      child: Container(
+        width: padding * 2,
+        height: padding * 2,
+        child: Center(
+            child: title != null
+                ? Text(
+                    '$title',
+                    style: TextStyle(
+                        color: textColor != null
+                            ? textColor
+                            : darkMode
+                                ? Colors.white
+                                : Colors.black,
+                        fontSize: 30),
+                  )
+                : Icon(
+                    icon,
+                    color: iconColor,
+                    size: 30,
+                  )),
+      ),
+    );
+  }
+}
+
+class NeuContainer extends StatefulWidget {
+  final bool darkMode;
+  final Widget child;
+  final BorderRadius borderRadius;
+  final EdgeInsetsGeometry padding;
+
+  NeuContainer(
+      {this.darkMode = false,
+      required this.child,
+      required this.borderRadius,
+      required this.padding});
+
+  @override
+  _NeuContainerState createState() => _NeuContainerState();
+}
+
+class _NeuContainerState extends State<NeuContainer> {
+  bool _isPressed = false;
+
+  void _onPointerDown(PointerDownEvent event) {
+    setState(() {
+      _isPressed = true;
+    });
+  }
+
+  void _onPointerUp(PointerUpEvent event) {
+    setState(() {
+      _isPressed = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    bool darkMode = widget.darkMode;
+    return Listener(
+      onPointerDown: _onPointerDown,
+      onPointerUp: _onPointerUp,
+      child: Container(
+        padding: widget.padding,
+        decoration: BoxDecoration(
+            color: darkMode ? colorDark : colorLight,
+            borderRadius: widget.borderRadius,
+            boxShadow: _isPressed
+                ? null
+                : [
+                    BoxShadow(
+                      color:
+                          darkMode ? Colors.black54 : Colors.blueGrey.shade200,
+                      offset: Offset(4.0, 4.0),
+                      blurRadius: 15.0,
+                      spreadRadius: 1.0,
+                    ),
+                    BoxShadow(
+                        color:
+                            darkMode ? Colors.blueGrey.shade700 : Colors.white,
+                        offset: Offset(-4.0, -4.0),
+                        blurRadius: 15.0,
+                        spreadRadius: 1.0)
+                  ]),
+        child: widget.child,
+      ),
+    );
+  }
+}
