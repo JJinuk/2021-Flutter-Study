a4ceb6d3774586fd0e98f145362c142cbd080cae
Jinuk <jitoor610@gmail.com>
2021-11-26 23:43:12 +0900

    [jinuk] 2021 GDSC Flutter Study - week9
    Signed-off-by: Jinuk <jitoor610@gmail.com>

--- Note/Jinuk/week9/calculator/lib/main.dart
+++ Note/Jinuk/week9/calculator/lib/main.dart
@@ -38,12 +38,112 @@ class _CalculatorNeuAppState extends State<CalculatorNeuApp> {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Container(
-                color: Colors.red,
-                child: Text('Result'),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    GestureDetector(
+                        onTap: () {
+                          setState(() {
+                            darkMode ? darkMode = false : darkMode = true;
+                          });
+                        },
+                        child: _switchMode()),
+                    SizedBox(height: 80),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        '6.010',
+                        style: TextStyle(
+                            fontSize: 55,
+                            fontWeight: FontWeight.bold,
+                            color: darkMode ? Colors.white : Colors.red),
+                      ),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            '=',
+                            style: TextStyle(
+                                fontSize: 35,
+                                color: darkMode ? Colors.green : Colors.grey),
+                          ),
+                          Text(
+                            '10+500*12',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: darkMode ? Colors.green : Colors.grey),
+                          )
+                        ]),
+                    SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
               ),
               Container(
                 child: Column(
                   children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonOval(title: 'sin'),
+                        _buttonOval(title: 'cos'),
+                        _buttonOval(title: 'tan'),
+                        _buttonOval(title: '%'),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonRounded(
+                            title: 'C',
+                            textColor:
+                                darkMode ? Colors.green : Colors.redAccent),
+                        _buttonRounded(title: '('),
+                        _buttonRounded(title: ')'),
+                        _buttonRounded(
+                            title: '/',
+                            textColor:
+                                darkMode ? Colors.green : Colors.redAccent),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonRounded(title: '7'),
+                        _buttonRounded(title: '8'),
+                        _buttonRounded(title: '9'),
+                        _buttonRounded(
+                            title: 'x',
+                            textColor:
+                                darkMode ? Colors.green : Colors.redAccent),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonRounded(title: '4'),
+                        _buttonRounded(title: '5'),
+                        _buttonRounded(title: '6'),
+                        _buttonRounded(
+                            title: '-',
+                            textColor:
+                                darkMode ? Colors.green : Colors.redAccent),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        _buttonRounded(title: '1'),
+                        _buttonRounded(title: '2'),
+                        _buttonRounded(title: '3'),
+                        _buttonRounded(
+                            title: '+',
+                            textColor:
+                                darkMode ? Colors.green : Colors.redAccent),
+                      ],
+                    ),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
@@ -71,30 +171,82 @@ class _CalculatorNeuAppState extends State<CalculatorNeuApp> {
 
   Widget _buttonRounded(
       {String? title,
-      double padding = 20,
+      double padding = 17,
       IconData? icon,
       Color? iconColor,
       Color? textColor}) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: NeuContainer(
+        darkMode: darkMode,
+        borderRadius: BorderRadius.circular(40),
+        padding: EdgeInsets.all(padding),
+        child: Container(
+          width: padding * 2,
+          height: padding * 2,
+          child: Center(
+              child: title != null
+                  ? Text(
+                      '$title',
+                      style: TextStyle(
+                          color: textColor != null
+                              ? textColor
+                              : darkMode
+                                  ? Colors.white
+                                  : Colors.black,
+                          fontSize: 30),
+                    )
+                  : Icon(icon, color: iconColor, size: 30)),
+        ),
+      ),
+    );
+  }
+
+  Widget _buttonOval({String? title, double padding = 17}) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: NeuContainer(
+        darkMode: darkMode,
+        borderRadius: BorderRadius.circular(50),
+        padding:
+            EdgeInsets.symmetric(horizontal: padding, vertical: padding / 2),
+        child: Container(
+          width: padding * 2,
+          child: Center(
+            child: Text(
+              '$title',
+              style: TextStyle(
+                  color: darkMode ? Colors.white : Colors.black,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _switchMode() {
     return NeuContainer(
       darkMode: darkMode,
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 15,
+      ),
       borderRadius: BorderRadius.circular(40),
-      padding: EdgeInsets.all(padding),
       child: Container(
-        width: padding * 2,
-        height: padding * 2,
-        child: Center(
-            child: title != null
-                ? Text(
-                    '$title',
-                    style: TextStyle(
-                        color: textColor != null
-                            ? textColor
-                            : darkMode
-                                ? Colors.white
-                                : Colors.black,
-                        fontSize: 30),
-                  )
-                : Icon(icon, color: iconColor, size: 30)),
+        width: 70,
+        child:
+            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+          Icon(
+            Icons.wb_sunny,
+            color: darkMode ? Colors.grey : Colors.redAccent,
+          ),
+          Icon(
+            Icons.nightlight_round,
+            color: darkMode ? Colors.green : Colors.grey,
+          ),
+        ]),
       ),
     );
   }
